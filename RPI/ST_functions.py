0ae61ea493bda9056c7b41143b46f2e56267565c
Samuel Gutierrez <samuel.gutierrez@usach.cl>
2018-09-10 23:45:54 -0300

    Developing new ST code.

--- RPI/ST_functions.py
+++ RPI/ST_functions.py
@@ -1,5 +1,4 @@
 # 1.- Imports
-
 import time
 import os
 import re
@@ -10,3 +9,44 @@ import numpy as np
 from PIL import Image
 from astropy.io import fits, ascii
 from astropy.table import Table
+
+
+# Check the initial data entered to the code.
+def rev_initial_data(sys_arg, len_arg):
+    if len_arg == 1:
+        print '- Too few arguments...'
+        print '- Add the full directory of the image.'
+        raise ValueError('Invalid data...')
+    elif len_arg == 2:
+        pic_name = sys_arg[1]
+        catalog_division = 10
+        print '- Using default catalog division (10 degrees).'
+    elif len_arg == 3:
+        pic_name = sys_arg[2]
+        if int(sys_arg[1]) == 5:
+            catalog_division = 5
+            print '- Catalog division: 5 degrees.'
+        else:
+            print '- Unknown catalog division.'
+            raise ValueError('Invalid data...')
+    elif len_arg > 3:
+        print '- Too many arguments.'
+        raise ValueError('Invalid data...')
+    else:
+        print '- Unknown error.'
+        raise ValueError('Invalid data...')
+    return pic_name, catalog_division
+
+
+# Generate '.fits' from '.jpg' image.
+def generate_fits(pic_name, fits_name):
+    image = Image.open(pic_name)
+    imagebw = image.convert('L')
+    xsize, ysize = imagebw.size
+    fits1 = imagebw.getdata()
+    fits2 = np.array(fits1)
+    fits3 = fits2.reshape(ysize, xsize)
+    fits4 = np.flipud(fits3)
+    fits5 = fits.PrimaryHDU(data=fits4)
+    fits5.writeto(fits_name, overwrite=True)
+    return
--- RPI/StarTracker_dev.py
+++ RPI/StarTracker_dev.py
@@ -1,43 +1,29 @@
-# 1.- Imports
-# import ST_functions
+# 1.- Imports.
+import ST_functions
 import os
 import sys
 
-# 2.- Define directories.
-DIR_root = os.path.dirname(os.path.abspath(__file__)) + '/'
+# 2.- Define directories and names.
+DIR_this = os.path.dirname(os.path.abspath(__file__)) + '/'
+DIR_img_fits = DIR_this
+DIR_stars = DIR_img_fits + 'sext'
+DIR_first_match = DIR_img_fits + 'new_cat'
+DIR_sext = './sextractor'
+DIR_proj_cat = './Catalog/Projected/'
+DIR_normal_cat = './Catalog/Normal/'
+fits_name = 'img_fits.fits'
 
-
+# 3.- Receives initial data.
 st_args = sys.argv
-if len(st_args) == 1:
-    pic_name = ' asdf '
-    catalog_division = 0
-    print '- Too few arguments.'
-    print '- Add the full directory to the image to analyze.'
-elif len(st_args) == 2:
-    pic_name = sys.argv[1]
-    catalog_division = 10
-    print '- Using default catalog division (10 degrees).'
-elif len(st_args) == 3:
-    pic_name = sys.argv[2]
-    catalog_division = sys.argv[1]
-    print '- Catalog division: ', int(catalog_division), 'degrees.'
-elif len(st_args) > 3:
-    pic_name = ' asdf '
-    catalog_division = 0
-    print '- Too many arguments.'
+len_arg = len(st_args)
+pic_name, cat_division = ST_functions.rev_initial_data(st_args, len_arg)
 
-
-#    pic_name = sys.argv[2]
-# except IndexError:
-#    pic_name = 'NO_NAME'
-#    print '- You need to add the full directory of the image to analyze.'
-#
-# try:
-#    catalog_division = sys.argv[1]
-# except IndexError:
-#    catalog_division = 10
-#    print '- Using default catalog division.'
-
-print 'ARGUMENTS:', st_args
+print '-.'*20
+print 'REVIEW:'
+print 'Initial arguments:', st_args
 print 'Image name:', pic_name
-print 'DIR: ', DIR_root
+print 'DIR_this: ', DIR_this
+print '-.'*20
+
+# 4.- Generate FITS image.
+ST_functions.generate_fits(pic_name, fits_name)
