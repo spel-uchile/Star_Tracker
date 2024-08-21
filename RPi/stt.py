94d273e9f6b41dbf52deb32c4573a57b026823ef
samuel-gutierrez <samuel.gutierrez@usach.cl>
2024-08-21 13:11:14 -0400

    Modifying stt.py script

--- RPi/stt.py
+++ RPi/stt.py
@@ -1,20 +1,25 @@
 import argparse
+import stt_selector as stt
 
 parser = argparse.ArgumentParser(prog="STT", description="STT main script")
 parser.add_argument("type", type=str, help="The way of running the software.")
 parser.add_argument("catalog", type=int, help="The degree of catalog separation in the algorithm.")
+parser.add_argument("-exp", "--exptime", type=int, help="Exposure time of the grabed picture (in direct mode).", default=800)
+parser.add_argument("-n", "--npic", type=int, help="The number of the picture to analyze (in sample mode).", default=1)
 args = parser.parse_args()
 stt_type = args.type
 cat_division = args.catalog
+n_pic = args.npic
+exp_time = args.exptime
 
 if cat_division not in (5, 10, 15):
     parser.error("Please introduce a valid catalog division: 5/10/15")
 
-if stt_type == "real_rpi":
-    print("a", cat_division)
+if stt_type == "direct_rpi":
+    stt.fn_a("direct_rpi", cat_division, exp_time)
 elif stt_type == "sample_rpi":
-    print("b", cat_division)
+    stt.fn_b("sample_rpi", cat_division, n_pic)
 elif stt_type == "sample_stereo":
-    print("c", cat_division)
+    stt.fn_c("sample_stereo", cat_division, n_pic)
 else:
-    raise NameError("---> ERROR: Please introduce a valid way to use this STT software")
+    raise NameError("---> ERROR: Please introduce a valid option to use this STT software (see Documentation)!")
