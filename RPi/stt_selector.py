bcac1b46b194641b1362f9880f9218964cc65fbf
samuel-gutierrez <samuel.gutierrez@usach.cl>
2024-08-28 12:25:26 -0400

    Minor fix

--- RPi/stt_selector.py
+++ RPi/stt_selector.py
@@ -6,11 +6,11 @@ file_dir = os.path.abspath(os.path.dirname(__file__))
 stt_dir = "{}/stt_data".format(file_dir)
 
 
-def solve_lis_grab_img(exposure_time, cat_division):
-    time_micros = 1000 * exposure_time
+def solve_lis_grab_img(cat_division, exposure_time_ms):
+    exptime_micros = 1000 * exposure_time_ms
     image_dir = "{}/stt_img.jpg".format(stt_dir)
-    print("---> STT: Taking picture with {} ms of exposure time".format(exposure_time))
-    task = "libcamera-still -o {} -t 1 --width 1024 --height 1024 --shutter {}".format(image_dir, time_micros)
+    print("\n---> STT: Taking picture with {} ms of exposure time.\n".format(exposure_time_ms))
+    task = "libcamera-still -o {} -t 1 --width 1024 --height 1024 --shutter {}".format(image_dir, exptime_micros)
     process = sp.Popen(task, shell=True, stdout=sp.PIPE)
     process.wait()
     return_code = process.returncode
@@ -21,7 +21,8 @@ def solve_lis_grab_img(exposure_time, cat_division):
 
 
 def solve_lis_sample_rpi(cat_division, n_pic):
-    print("---> STT: Analyzing picture from Sample_images/RPi/img_{}.jpg\n".format(n_pic))
+    print("\n---> STT: Analyzing picture from Sample_images/RPi/img_{}.jpg "
+          "using a catalog division of {}.\n".format(n_pic, cat_division))
     image_dir = "{}/Sample_images/RPi/img_{}.jpg".format(file_dir, n_pic)
     stt.solve_lis(image_dir, cat_division, stt_dir)
     return 0
